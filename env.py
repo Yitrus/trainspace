9565a13fb6c7f0918b9f34b94383553a1945a668
xmu <citrusyi@163.com>
2024-04-30 19:11:27 +0800

    新阶段的训练

--- env.py
+++ env.py
@@ -8,17 +8,26 @@ class Kernel():
     def __init__(self):
         self.last_stat = 10
         # self.action_space = [0, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
-        self.action_space = [0, 4096, 8192, 16384, 32768, 65536]
+        # basic page also should try migrate more
+        # self.action_space = [8192, 16384, 32768, 65536, 131072, 262144]
+        
+        # below are huge page
+        # self.action_space = [0, 4096, 8192, 16384, 32768, 65536, 131072]
+        self.action_space = [0, 16384, 32768, 65536, 131072, 262144]
         self.n_actions = len(self.action_space)
 
     def read_sample(self):
-        cat_code = os.popen('cat /sys/fs/cgroup/htmm/memory.hit_ratio_show').read()
-        numbers = cat_code.split()
-        print("ratio " + numbers[0])
-        print("others " + numbers[1])
-        print("dram " + numbers[2])
-        print("pm " + numbers[3])
-        return int(numbers[0])/10
+        while True:
+            cat_code = os.popen('cat /sys/fs/cgroup/htmm/memory.hit_ratio_show').read()
+            numbers = cat_code.split()
+            print("ratio " + numbers[0])
+            print("others " + numbers[1])
+            print("dram " + numbers[2])
+            print("pm " + numbers[3])
+            if int(numbers[2]) == 0 and int(numbers[3]) == 0:
+                time.sleep(10)
+            else:
+                return int(numbers[0])/10
 
     def reset(self):
         status = self.read_sample()
@@ -26,6 +35,8 @@ class Kernel():
         return status
 
     def step(self, action):
+        if action==0:
+            pass
         action *= 4096
         command = 'echo "{}" > /sys/fs/cgroup/htmm/memory.action_show'.format(str(action))
         try:
@@ -35,7 +46,7 @@ class Kernel():
         except Exception as e:
             print("action failed")
 
-        time.sleep(20)
+        time.sleep(10)
 
         status = self.read_sample()
         reward = status - self.last_stat
--- qtable.py
+++ qtable.py
@@ -5,12 +5,13 @@ import pandas as pd
 
 
 class QLearningTable:
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.5):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.6):
         self.actions = actions  # a list
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        # self.q_table = pd.read_csv('./data.txt', sep='\s+', dtype=np.float64)
 
     def choose_action(self, observation):
         self.check_state_exist(observation)
